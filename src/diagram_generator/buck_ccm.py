5724888d024b31ccad3d99dae1c448dd7e70f3fb
Daniel Puratich <dpuratich@uwaterloo.ca>
2025-01-13 23:08:11 -0800

    added in image for buck

--- src/diagram_generator/buck_ccm.py
+++ src/diagram_generator/buck_ccm.py
@@ -8,35 +8,42 @@ TITLE = "Buck Converter in Continuous Conduction Mode"
 def plotter() :
     NUM_POINTS = 1000
     SWITCHING_FREQUENCY = 3 
-    VIN = 12
+    VIN = 12.0
     VOUT = 3.3
+
+    duty_cycle = VOUT / VIN
+    negative_duty_cycle = 1.0 - duty_cycle
+    period = 1.0 / SWITCHING_FREQUENCY
+    on_time = duty_cycle * period
+    off_time = (1 - duty_cycle) * period
 
     t = np.linspace(0, 1, NUM_POINTS) # time vector
 
-    square_wave = np.sign(np.sin(2 * np.pi * SWITCHING_FREQUENCY * t))
-    triangle_wave = 2 * np.abs(2 * (t * SWITCHING_FREQUENCY % 1) - 1) - 1
+    square_wave =  ((t * SWITCHING_FREQUENCY % 1) < (duty_cycle)).astype(float)
+    v_sw = VIN * square_wave + 0
 
-    v_sw = 5 * square_wave + 0
-    i_l = 1 * triangle_wave + 2
-
-    # i_in =
-    # v_in = 
-    # v_out = 
+    triangle_wave = np.mod(t * SWITCHING_FREQUENCY, 1)
+    triangle_wave = np.where(
+        triangle_wave < duty_cycle,
+        2 * triangle_wave / duty_cycle,  # Rising part
+        2 * (1 - triangle_wave) / (1 - duty_cycle)  # Falling part
+    )
+    i_l = 1 * triangle_wave + 4
 
     plt.figure(figsize=(10, 6))
 
     plt.plot(t, v_sw, label="V_sw")
     plt.plot(t, i_l, label="I_l")
-    # plt.plot(t, )
-    # plt.plot(t, )
-    # plt.plot(t, )
+    # plt.plot(t, ) # i_in
     plt.axhline(y=VIN, color='r', linestyle='--', label=f"V_in")
     plt.axhline(y=VOUT, color='r', linestyle='--', label=f"V_out")
 
     plt.title(f"{TITLE}")
+    plt.gca().axes.get_xaxis().set_visible(False)
     plt.xlabel("Time")
     # plt.ylabel("")
-    plt.legend()
+    # plt.legend()
+    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
     plt.grid(True)
 
 if __name__ == "__main__":
