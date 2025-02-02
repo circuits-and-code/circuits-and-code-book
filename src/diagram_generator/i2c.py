cf4efa0b01db4a9d304e1087729a6163e92bf518
Sahil Kale <32375512+sahil-kale@users.noreply.github.com>
2025-02-02 11:16:44 -0500

    Fix plot todo (#179)
    * Fix plot todo
    
    * Make LED more asthetic

--- src/diagram_generator/i2c.py
+++ src/diagram_generator/i2c.py
@@ -4,30 +4,31 @@ import argparse
 
 TITLE = "I2C SCL"
 
-def plotter() :
+
+def plotter():
     kilo = 1e3
     nano = 1e-9
     pico = 1e-12
 
     # trying to be realistic with values
     freq = 400 * kilo
-    tau = 2.2 * kilo * 47 * pico 
+    tau = 2.2 * kilo * 47 * pico
     v_ll = 3.3
 
     period = 1.0 / float(freq)
-    half_period = period / 2 
+    half_period = period / 2
 
     number_of_plotted_points = 5000
     cycles_shown = 3
-    t = np.linspace(0, cycles_shown * period, number_of_plotted_points) 
+    t = np.linspace(0, cycles_shown * period, number_of_plotted_points)
 
     scl = np.zeros_like(t)
     for i, t_i in enumerate(t):
         cycle_pos = t_i % period  # Time within each cycle
-        if cycle_pos < half_period :
+        if cycle_pos < half_period:
             scl[i] = 0
-        else :
-            scl[i] = v_ll * (1 - np.exp((-1* (cycle_pos-half_period))/tau))
+        else:
+            scl[i] = v_ll * (1 - np.exp((-1 * (cycle_pos - half_period)) / tau))
 
     plt.figure(figsize=(10, 6))
     plt.title(f"{TITLE}")
@@ -37,6 +38,7 @@ def plotter() :
     plt.ylabel("Voltage (V)")
     plt.legend()
     plt.grid(True)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(f"{TITLE} Plotter")
--- src/diagram_generator/i2c_comparison.py
+++ src/diagram_generator/i2c_comparison.py
@@ -4,37 +4,42 @@ import argparse
 
 TITLE = "I2C SCL with Varying Pullup Resistance"
 
-def plotter() :
+
+def plotter():
     kilo = 1e3
     pico = 1e-12
 
     freq = 400 * kilo
     v_ll = 3.3
 
-    tau = 2.2 * kilo * 47 * pico 
+    tau = 2.2 * kilo * 47 * pico
     tau_higher = 4.7 * kilo * 47 * pico
     tau_lower = 470 * 47 * pico
 
     period = 1.0 / float(freq)
-    half_period = period / 2 
+    half_period = period / 2
 
     number_of_plotted_points = 5000
     cycles_shown = 3
-    t = np.linspace(0, cycles_shown * period, number_of_plotted_points) 
+    t = np.linspace(0, cycles_shown * period, number_of_plotted_points)
 
     scl = np.zeros_like(t)
     scl_higher = np.zeros_like(t)
     scl_lower = np.zeros_like(t)
     for i, t_i in enumerate(t):
         cycle_pos = t_i % period  # Time within each cycle
-        if cycle_pos < half_period :
+        if cycle_pos < half_period:
             scl[i] = 0
             scl_higher[i] = 0
             scl_lower[i] = 0
-        else :
-            scl[i] = v_ll * (1 - np.exp((-1* (cycle_pos-half_period))/tau))
-            scl_higher[i] = v_ll * (1 - np.exp((-1* (cycle_pos-half_period))/tau_higher))
-            scl_lower[i] = v_ll * (1 - np.exp((-1* (cycle_pos-half_period))/tau_lower))
+        else:
+            scl[i] = v_ll * (1 - np.exp((-1 * (cycle_pos - half_period)) / tau))
+            scl_higher[i] = v_ll * (
+                1 - np.exp((-1 * (cycle_pos - half_period)) / tau_higher)
+            )
+            scl_lower[i] = v_ll * (
+                1 - np.exp((-1 * (cycle_pos - half_period)) / tau_lower)
+            )
 
     plt.figure(figsize=(10, 6))
     plt.title(f"{TITLE}")
@@ -46,6 +51,7 @@ def plotter() :
     plt.ylabel("Voltage (V)")
     plt.legend()
     plt.grid(True)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(f"{TITLE} Plotter")
