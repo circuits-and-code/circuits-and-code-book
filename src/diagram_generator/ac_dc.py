a16aab61df02c12b38d08754437ea58d39b6386c
Daniel Puratich <dpuratic@uwaterloo.ca>
2025-03-16 11:17:03 -0300

    Added coupling section to oscilloscope question

---
+++ src/diagram_generator/ac_dc.py
@@ -0,0 +1,52 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import argparse
+
+# fmt: off
+
+TITLE = "AC vs DC Coupling"
+
+def plotter() :
+
+    NUM_PTS = 5000
+    t = np.linspace(0, 1, NUM_PTS)
+    YLIM_SCALE = 1.2
+
+    FREQ = 5
+    OFFSET = 2
+    AMPLITUDE = 1
+
+    # TODO : draw a sinusoid with a DC offset
+    ac = AMPLITUDE * np.sin(2 * np.pi * FREQ * t)
+    full = ac + OFFSET
+
+    fig, ax = plt.subplots(2, 1, figsize=(8, 6), sharex=True)
+
+    ax[0].plot(t, full, label="DC Coupled")
+    ax[0].axhline(y=OFFSET, color='r', linestyle='--', label=f"DC Component")
+    ax[0].set_title(f"{TITLE}")
+    ax[0].set_ylim(0, (AMPLITUDE+OFFSET)*YLIM_SCALE) 
+    ax[0].set_xlabel("Time (seconds)")
+    ax[0].set_ylabel("Voltage (volts)")
+    ax[0].legend()
+
+    ax[1].plot(t, ac, label="AC Coupled")
+    ax[1].set_ylim(-AMPLITUDE*YLIM_SCALE, AMPLITUDE*YLIM_SCALE) 
+    ax[1].set_xlabel("Time (seconds)")
+    ax[1].set_ylabel("Voltage (volts)")
+    ax[1].legend()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(f"{TITLE} Plotter")
+    parser.add_argument("--output_path", help="Path to save image to")
+
+    args = parser.parse_args()
+
+    plotter()
+
+    if args.output_path is not None:
+        plt.savefig(args.output_path)
+    else:
+        plt.show()
+
+# fmt: on
